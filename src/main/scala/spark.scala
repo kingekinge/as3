15910053ee2bbbb56fa2117d6ef5ff086cf18539
liangkai <kingekinge@163.com>
2016-05-30 00:35:57 +0800

    求文件单词数

--- src/main/scala/spark.scala
+++ src/main/scala/spark.scala
@@ -20,6 +20,7 @@ object spark extends App{
 //  }).distinct().map(x=>(x._1,1)).reduce((x,y)=>x._2+y._2)
 //  println(map)
 
+  //求一个文件中所有的字母数
 //  val p= sc.textFile("C:\\Users\\zhang\\Desktop\\README.md").flatMap(_.toCharArray)
 //    .filter(x=>(x>='A'&&x<='Z'||x>='a'&&x<='z'))
 //  .map((_,1)).reduceByKey(_+_)
@@ -42,6 +43,8 @@ object spark extends App{
 private val rdd: RDD[String] = sc.textFile("hdfs://192.168.40.102:9000/wordcount/input")
   private val buffer: mutable.Buffer[(String, Int)] = rdd.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).collect().toBuffer
   println(buffer)
+
+
 }
 
 
