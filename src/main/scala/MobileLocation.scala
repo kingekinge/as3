d71bc1f54d78e4ad0ee0359a61a2533fc53ec6f3
liangkai <kingekinge@163.com>
2016-05-30 00:51:39 +0800

    new test for branch

--- src/main/scala/MobileLocation.scala
+++ src/main/scala/MobileLocation.scala
@@ -60,5 +60,6 @@ object MobileLocation {
 
 
 
+
   }
 }
--- src/main/scala/spark.scala
+++ src/main/scala/spark.scala
@@ -20,7 +20,7 @@ object spark extends App{
 //  }).distinct().map(x=>(x._1,1)).reduce((x,y)=>x._2+y._2)
 //  println(map)
 
-  //求一个文件中所有的字母数
+  //求一个文件中所有的字母
 //  val p= sc.textFile("C:\\Users\\zhang\\Desktop\\README.md").flatMap(_.toCharArray)
 //    .filter(x=>(x>='A'&&x<='Z'||x>='a'&&x<='z'))
 //  .map((_,1)).reduceByKey(_+_)
@@ -44,6 +44,8 @@ private val rdd: RDD[String] = sc.textFile("hdfs://192.168.40.102:9000/wordcount
   private val buffer: mutable.Buffer[(String, Int)] = rdd.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).collect().toBuffer
   println(buffer)
 
+  private val builder: StringBuilder = new StringBuilder
+  private val append: StringBuilder = builder.append("bd")
 
 }
 
